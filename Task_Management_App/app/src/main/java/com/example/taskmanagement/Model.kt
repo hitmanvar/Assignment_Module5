5e24578d296be4bf384aa11ada307eb6f675e134
hitmanvar <141495944+hitmanvar@users.noreply.github.com>
2023-12-26 16:48:09 +0530

    Task Management App

---
+++ Task_Management_App/app/src/main/java/com/example/taskmanagement/Model.kt
@@ -0,0 +1,17 @@
+package com.example.taskmanagement
+
+
+
+
+class Model
+{
+    var id = 0
+    var task_name= ""
+    var task_description= ""
+    var task_date= ""
+    var task_time= ""
+    var task_priority= ""
+
+
+
+}
